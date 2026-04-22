7f4b89ef7447127be8214a33ad67e1ddb25d7554
tommylynnnnn <hildabaggin@gmail.com>
2026-04-21 21:21:31 -0400

    Add stats to queen objects in script.js

--- script.js
+++ script.js
@@ -1,14 +1,109 @@
 const ALL_QUEENS = [
-    { name: "Alaska", img: "https://i.pinimg.com/736x/6e/ef/ca/6eefcadaa26067a62884227350d683b8.jpg" },
-    { name: "Alyssa Edwards", img: "https://i.pinimg.com/736x/0b/bd/51/0bbd517dfcbcb21aacb0fa05f0af9809.jpg" },
-    { name: "Bob the Drag Queen", img: "https://i.pinimg.com/736x/71/19/5a/71195a08be0d3a56bad3427f517901e4.jpg" },
-    { name: "Katya", img: "https://i.pinimg.com/736x/e9/0f/f6/e90ff66a78ec1d4bdb7bd06ee320a5d2.jpg" },
-    { name: "Sasha Velour", img: "https://i.pinimg.com/736x/7c/6d/a4/7c6da4478b5625c83e74c55a542e5e95.jpg" },
-    { name: "Bianca Del Rio", img: "https://i.pinimg.com/736x/26/43/61/264361d07c867a88630c4a4986d0c3b3.jpg" },
-    { name: "Jinkx Monsoon", img: "https://i.pinimg.com/736x/18/c1/d1/18c1d14cdcdf348000446f1a1cfb8e8b.jpg" },
-    { name: "Onya Nurve", img: "https://i.pinimg.com/736x/a7/04/a1/a704a15997585ec88a4d24f4ab1db791.jpg" }
-];
+    {
+    name: "Alaska",
+    img: "https://i.pinimg.com/736x/6e/ef/ca/6eefcadaa26067a62884227350d683b8.jpg",
+    stats: {
+        acting: 9,
+        comedy: 8,
+        improv: 10,
+        dance: 9,
+        design: 8,
+        lipsync: 7
+    }
+},
 
+    {
+    name: "Alyssa Edwards",
+    img: "https://i.pinimg.com/736x/0b/bd/51/0bbd517dfcbcb21aacb0fa05f0af9809.jpg",
+    stats: {
+        acting: 9,
+        comedy: 8,
+        improv: 10,
+        dance: 9,
+        design: 8,
+        lipsync: 7
+    }
+},
+
+    {
+    name: "Bob the Drag Queen",
+    img: "https://i.pinimg.com/736x/71/19/5a/71195a08be0d3a56bad3427f517901e4.jpg",
+    stats: {
+        acting: 9,
+        comedy: 8,
+        improv: 10,
+        dance: 9,
+        design: 8,
+        lipsync: 7
+    }
+},
+    
+    {
+    name: "Katya",
+    img: "https://i.pinimg.com/736x/e9/0f/f6/e90ff66a78ec1d4bdb7bd06ee320a5d2.jpg",
+    stats: {
+        acting: 9,
+        comedy: 8,
+        improv: 10,
+        dance: 9,
+        design: 8,
+        lipsync: 7
+    }
+},
+
+    {
+    name: "Sasha Velour",
+    img: "https://i.pinimg.com/736x/7c/6d/a4/7c6da4478b5625c83e74c55a542e5e95.jpg",
+    stats: {
+        acting: 9,
+        comedy: 8,
+        improv: 10,
+        dance: 9,
+        design: 8,
+        lipsync: 7
+    }
+},
+    
+    {
+    name: "Bianca Del Rio",
+    img: "https://i.pinimg.com/736x/26/43/61/264361d07c867a88630c4a4986d0c3b3.jpg",
+    stats: {
+        acting: 9,
+        comedy: 8,
+        improv: 10,
+        dance: 9,
+        design: 8,
+        lipsync: 7
+    }
+},
+
+    {
+    name: "Jinkx Monsoon",
+    img: "https://i.pinimg.com/736x/18/c1/d1/18c1d14cdcdf348000446f1a1cfb8e8b.jpg",
+    stats: {
+        acting: 9,
+        comedy: 8,
+        improv: 10,
+        dance: 9,
+        design: 8,
+        lipsync: 7
+    }
+},
+
+    {
+    name: "Onya Nurve",
+    img: "https://i.pinimg.com/736x/a7/04/a1/a704a15997585ec88a4d24f4ab1db791.jpg",
+    stats: {
+        acting: 9,
+        comedy: 8,
+        improv: 10,
+        dance: 9,
+        design: 8,
+        lipsync: 7
+    }
+}
+    
+];
 
 const grid = document.getElementById("queen-grid");
 
