28c15e282e35a3ea9e4276d7f009d0c5762ed245
tommylynnnnn <hildabaggin@gmail.com>
2026-04-22 12:31:59 -0400

    Fix formatting issues in script.js

--- script.js
+++ script.js
@@ -65,6 +65,11 @@ const ALL_QUEENS = [
         name: "Juicy Love Dion",
         img: "https://i.pinimg.com/736x/c1/a9/d9/c1a9d95922919fc9fc361072247c7a84.jpg",
         stats: { acting: 6, comedy: 5, improv: 6, dance: 9, design: 10, lipsync: 11 }
+    },
+    {
+        name: "Patricia Shen",
+        img: "https://s3.amazonaws.com/nightjarprod/content/uploads/sites/118/2020/09/09165555/BECOMING-EDDIE_Still_1-1000x600.png",
+        stats: { acting: 6, comedy: 5, improv: 6, dance: 9, design: 10, lipsync: 11 }
     }
 ];
 
